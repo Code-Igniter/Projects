ce7afb6a319f15f060eb9fd98b66e95a03e8b12d
Weebn't Programmer <89193581+RzCodez@users.noreply.github.com>
2021-10-08 08:49:13 +0800

    Delete tes-17-blu-arsip-landing-page directory

--- tes-17-blu-arsip-landing-page/scripts/hamburger.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu input');
-const nav = document.querySelector('header nav ul');
-
-menuToggle.addEventListener('click', function(){
-    nav.classList.toggle('slide');
-})
